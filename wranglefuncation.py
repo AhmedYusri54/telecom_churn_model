44616177d628b5011462a220464bebf43bbf3a1f
AhmedYusri54 <122183301+AhmedYusri54@users.noreply.github.com>
2025-04-21 18:39:59 +0200

    Update wranglefuncation.py

--- wranglefuncation.py
+++ wranglefuncation.py
@@ -11,19 +11,9 @@ def wrangle(filepath: str):
     A Cleaned dataframe based on the insights from analysis.
     """
     # Load the data and check for the path if it correct 
-    try:
-        df = pd.read_csv(filepath)
-    except FileNotFoundError:
-        print("Invalid filepath try a vaild one...")
-    else:
-        # print("DataLoaded and will been process")
-        pass
-        
+    df = pd.read_csv(filepath) 
     # 1. Drop the unused columns 
     df.drop(columns=["customer_id", "date_of_registration", "pincode"], inplace=True)
-    
-    # 2. Change the targets columns from object type into int type 
-    df["gender"] = df["gender"].map({"F": 0, "M": 1})
     
     # 3. Processing the false values (Negative) values in the data_used column
     mean_value = round(df["data_used"].mean())
